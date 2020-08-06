7a0774b92bf99e81919cbd06a73e2cca46407634
Onu Monday Emmanuel <mondayemmanuel67@gmail.com>
2020-08-06 14:55:13 +0100

    Updates

---
+++ precache-manifest.04074847dc34494e8df5198f1d9c99de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feac8069058dca620755cbed5139d6e9",
+    "url": "/restaurant-web/index.html"
+  },
+  {
+    "revision": "77dc7fe8b7a1e36fa06d",
+    "url": "/restaurant-web/static/css/main.1063b0b0.chunk.css"
+  },
+  {
+    "revision": "439f946b2b4e69ce1a6b",
+    "url": "/restaurant-web/static/js/2.db3c6044.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/restaurant-web/static/js/2.db3c6044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77dc7fe8b7a1e36fa06d",
+    "url": "/restaurant-web/static/js/main.4beb19c2.chunk.js"
+  },
+  {
+    "revision": "0a60edc1dfa03c0e492d",
+    "url": "/restaurant-web/static/js/runtime-main.ebe38fcf.js"
+  },
+  {
+    "revision": "eb045dbb13d50bf9975f8da4dbaff1a0",
+    "url": "/restaurant-web/static/media/food.eb045dbb.jpg"
+  },
+  {
+    "revision": "99296a235597c7fe48cf51dde3bfa20c",
+    "url": "/restaurant-web/static/media/food5.99296a23.png"
+  }
+]);
